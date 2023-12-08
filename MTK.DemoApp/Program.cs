848bbe59c14c5b2e9522c2755bfce36c60266ac7
jcfnomada <jferreira.bluelight@outlook.pt>
2023-12-08 20:38:39 +0000

    Add pending files to Source Control

---
+++ MTK.DemoApp/Program.cs
@@ -0,0 +1,19 @@
+using CommunityToolkit.Mvvm.Messaging;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using MTK.Blazor;
+using MTK.DemoApp;
+using MTK.DemoApp.ViewModels;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+// MTK
+builder.Services.AddSingleton<IMessenger>(_ => WeakReferenceMessenger.Default);
+builder.Services.AddMtk();
+builder.Services.AddTransient<CounterViewModel>();
+
+await builder.Build().RunAsync();
